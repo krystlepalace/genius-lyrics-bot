f75f7d2462b03c39cc9acc9ca150c26e5df339d1
Fvbvke <novoselcevila12@gmail.com>
2023-09-12 19:21:24 +0300

    Bug fixes and better structure

--- handlers/songs_inline.py
+++ handlers/songs_inline.py
@@ -1,11 +1,12 @@
 from uuid import uuid4
-
+import main
 from aiogram import Router
 from aiogram.types import InlineQuery, InlineQueryResultArticle, \
-    InputTextMessageContent
+    InputTextMessageContent, ChosenInlineResult
 import lyricsgenius as lg
 from utils.genius_api import LyricsFinder
 from config import CONFIG
+from keyboards import song_url
 
 router = Router()
 finder = LyricsFinder()
@@ -18,12 +19,28 @@ async def show_results(inline_query: InlineQuery):
 
     results = []
     for song in songs:
+        iddd= str(song['result']['id'])
+        print(f"Result id N: {iddd}")
         results.append(InlineQueryResultArticle(
-            id=str(uuid4()),
+            id=str(song['result']["id"]),
             title=song['result']["title"],
             description=song["result"]["artist_names"],
             input_message_content=InputTextMessageContent(
-                message_text=str(genius.lyrics(song["result"]["id"]))
-            )
+                message_text="Loading text...",
+            ),
+            reply_markup=song_url.song_url_button(url="genius.com",
+                                  title=song['result']["full_title"]).as_markup()
         ))
     await inline_query.answer(results, is_personal=True)
+
+
+@router.chosen_inline_result()
+async def load_lyrics(
+        chosen_result: ChosenInlineResult,
+):
+    print(f"Result id {chosen_result.result_id}")
+    l = str(genius.lyrics(int(chosen_result.result_id)))
+    message = await main.bot.edit_message_text(
+        inline_message_id=chosen_result.inline_message_id,
+        text=l
+    )
