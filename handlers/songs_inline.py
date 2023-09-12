84efbf8022d4e9f614785d5bcc07bda948b6174b
Fvbvke <novoselcevila12@gmail.com>
2023-09-12 18:32:14 +0300

    Trying to impl;ement InlineQuery handler

---
+++ handlers/songs_inline.py
@@ -0,0 +1,27 @@
+from aiogram import Router
+from aiogram.types import InlineQuery, InlineQueryResultArticle, \
+    InputTextMessageContent
+import lyricsgenius as lg
+from utils.genius_api import LyricsFinder
+from config import CONFIG
+
+router = Router()
+finder = LyricsFinder()
+genius = lg.Genius(CONFIG.genius_token.get_secret_value(), timeout=40)
+
+
+@router.inline_query()
+async def show_results(inline_query: InlineQuery):
+    songs = genius.search_songs(inline_query.query, per_page=5, page=1)['hits']
+
+    results = []
+    for song in songs:
+        results.append(InlineQueryResultArticle(
+            id=str(song["result"]["id"]),
+            title=song['result']["title"],
+            description=song["result"]["artist_names"],
+            input_message_content=InputTextMessageContent(
+                message_text=str(genius.lyrics(song["result"]["id"]))
+            )
+        ))
+    await inline_query.answer(results, is_personal=True)
